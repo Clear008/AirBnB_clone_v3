df88b2b45bc4854ab835e1dc9f10b4e15b271efb
Ilias Chokairi <iliaschokairi@gmail.com>
2024-04-26 12:17:51 +0100

    forget CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,6 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''The index view for the API.'''
+"""The index view for the API."""
 
 from api.v1.views import app_views
 from flask import jsonify
