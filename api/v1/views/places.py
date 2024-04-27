08915c948e8f13937fc320bf17da1d6b9c001a46
Ilias Chokairi <iliaschokairi@gmail.com>
2024-04-27 11:11:45 +0100

    push places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""
-A new view for places
-"""
+"""create a places view """
 
+from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
 from models.city import City
 from models.place import Place
 from models.user import User
-from flask import jsonify, abort, request
 
 
 @app_views.route('/cities/<city_id>/places',
                  methods=['GET', 'POST', 'PUT', 'DELETE'],
                  strict_slashes=False)
 def places_by_city(city_id):
-    """places by city"""
+    """get places by city"""
     city = storage.get(City, city_id)
     if not city:
         abort(404)
@@ -45,7 +43,7 @@ def places_by_city(city_id):
                  methods=['GET', 'POST', 'PUT', 'DELETE'],
                  strict_slashes=False)
 def places(place_id):
-    """place view by their place_id"""
+    """place by her place_id"""
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
@@ -59,13 +57,13 @@ def places(place_id):
         data = request.get_json(silent=True)
         if not data:
             return jsonify("Not a JSON"), 400
-        keys_to_ignore = ["id",
-                          "user_id",
-                          "city_id",
-                          "created_at",
-                          "updated_at"]
+        key_to_ignore = ["id",
+                         "user_id",
+                         "city_id",
+                         "created_at",
+                         "updated_at"]
         for key, value in data.items():
-            if key not in keys_to_ignore:
+            if key not in key_to_ignore:
                 setattr(place, key, value)
         storage.save()
         return jsonify(place.to_dict()), 200
