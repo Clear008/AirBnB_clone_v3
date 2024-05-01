1835d80f75f59d9d7abd21624fe2b5a883892ab3
Ilias Chokairi <iliaschokairi@gmail.com>
2024-05-01 11:11:53 +0100

    add get method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@
 """my route is register in this index"""
 
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, request
 from models import storage
 from models.state import State
 from models.city import City
@@ -19,29 +19,23 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     """Returns the number of objects by name"""
-    classes = {
-        City: "cities",
-        State: "states",
-        Amenity: "amenities",
-        User: "users",
-        Place: "places",
-        Review: "reviews"
-    }
-    list_of_counts = {}
-    for key in classes.keys():
-        count = storage.count(key.__name__)
-        if key.__name__ == City:
-            list_of_counts["cities"] = count
-        if key.__name__ == State:
-            list_of_counts["states"] = count
-        if key.__name__ == Amenity:
-            list_of_counts["amenities"] = count
-        if key.__name__ == User:
-            list_of_counts["places"] = count
-        if key.__name__ == Review:
-            list_of_counts["reviews"] = count
-    return jsonify(list_of_counts)
-        
+    if request.method == 'GET':
+        classes = {
+            City: "cities",
+            State: "states",
+            Amenity: "amenities",
+            User: "users",
+            Place: "places",
+            Review: "reviews"
+        }
+        count_by_type = {}
+        for key, value in classes.items():
+            try:
+                count = storage.count(key)
+            except Exception as e:
+                count = 0
+            count_by_type[value] = count
+        return jsonify(count_by_type)
